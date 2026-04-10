49d55e9ae965a21708c9d1562da0d770e69e2c9c
bansi740 <bansitalaviya740@gmail.com>
2026-04-10 11:11:14 +0530

    initial commit

--- lib/ui/screen/home/home_screen.dart
+++ lib/ui/screen/home/home_screen.dart
@@ -74,7 +74,7 @@ class _HomeScreenState extends State<HomeScreen>
       backgroundColor: const Color(0xffF8FAFC),
       // Gradient AppBar
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(56),
+        preferredSize: const Size.fromHeight(50),
         child: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
